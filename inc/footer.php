23dc66031c3c4dbd8b5d524eba1dd68ab0a41c11
Nazm,ul Hossain <70949738+im-nazmul@users.noreply.github.com>
2020-12-19 19:56:34 +0600

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+        <footer class="well">
+            <h3 style="text-align:center">&copy; All Right Reserved</h3>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP OOP &amp; PDO Dynamic CRUD</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="well text-center">
+            <h1>Dynamic CRUD with PHP OOP &amp; PDO</h1>
+        </div>
